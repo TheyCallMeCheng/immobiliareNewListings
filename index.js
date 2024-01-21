e2f9088ec1e7cf7d1ad6159def9bbe7372daebfa
Theycallmecheng <cheng_crop@protonmaill.com>
2024-01-21 21:02:28 +0100

    Feat: added telegram functionality

--- index.js
+++ index.js
@@ -18,6 +18,51 @@ const queryParams = {
 
 const botToken = process.env.BOT_TOKEN;
 const chatId = process.env.CHAT_ID;
+const lowPriceIndex = 100;
+
+function sendTelegramMessage(newListings) {
+    const telegramApiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+    const message = newListings.map(item => {
+        if (parseFloat(item.realEstate.price.value) > lowPriceIndex) {
+            return `
+${item.realEstate.title}
+🚀  Link: ${item.seo.url}
+💸  Price: ${item.realEstate.price.formattedValue}
+📍   Position: ${item.realEstate.properties[0].location.macrozone}
+🗺️  G Maps: https://maps.google.com/?q=${item.realEstate.properties[0].location.latitude},${item.realEstate.properties[0].location.longitude}
+🌆  Floor: ${item.realEstate.properties[0].floor.value}
+🚾  Bathrooms ${item.realEstate.properties[0].bathrooms}
+            `;
+        } else {
+            return `
+📌💥📢📌💥📢📌💥📢📌💥📢
+Prezzo "Basso" (<${lowPriceIndex})
+${item.realEstate.title}
+🚀  Link: ${item.seo.url}
+💸  Price: ${item.realEstate.price.formattedValue}
+📍   Position: ${item.realEstate.properties[0].location.macrozone}
+🗺️  G Maps: https://maps.google.com/?q=${item.realEstate.properties[0].location.latitude},${item.realEstate.properties[0].location.longitude}
+🌆  Floor: ${item.realEstate.properties[0].floor.value}
+🚾  Bathrooms ${item.realEstate.properties[0].bathrooms}
+📌💥📢📌💥📢📌💥📢📌💥📢
+            `;
+        }
+    }).join('\n');
+
+    const telegramParams = {
+        chat_id: chatId,
+        text: message
+    };
+
+
+    axios.post(telegramApiUrl, telegramParams)
+        .then(response => {
+            console.log('Telegram message sent:', response.data);
+        })
+        .catch(error => {
+            console.error('Error sending Telegram message:', error);
+        });
+}
 
 function readPreviousResponse() {
     try {
@@ -38,24 +83,18 @@ axios.get(apiUrl, { params: queryParams })
     .then(response => {
         const currentResponse = response.data.results; // Assuming 'data' is the property containing the array
         const previousResponse = readPreviousResponse();
-        console.log(currentResponse)
-        writeCurrentResponse(currentResponse);
+        //        writeCurrentResponse(currentResponse);
 
         // Compare the current and previous responses
         const newListings = currentResponse.filter(currentItem => {
-            console.log(currentItem.realEstate.id)
-            return !previousResponse.some(prevItem => {
-                console.log("Prev:", prevItem.realEstate.id)
-                return prevItem.realEstate.id === currentItem.realEstate.id
-            });
-
+            return !previousResponse.some(prevItem => prevItem.realEstate.id === currentItem.realEstate.id);
         });
 
         if (newListings.length > 0) {
             console.log('New listings detected!');
 
             // Create a scatter plot with the new data
-            createScatterPlot(currentResponse);
+            //createScatterPlot(currentResponse);
 
             // Save the current response for future comparison
             writeCurrentResponse(currentResponse);
